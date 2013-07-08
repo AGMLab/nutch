de3b78840515fe9de9fad0ed90955757a74b48a3
Ahmet Emre Aladağ <emre.aladag@agmlab.com>
2013-07-08 15:54:29 +0300

    LinkRank integration to Nutch

---
+++ src/java/org/apache/nutch/scoring/LinkRankGiraphJob.java
@@ -0,0 +1,76 @@
+package org.apache.nutch.scoring;
+
+import org.apache.giraph.conf.GiraphConfiguration;
+import org.apache.giraph.edge.ByteArrayEdges;
+import org.apache.giraph.examples.LinkRank.LinkRankComputation;
+import org.apache.giraph.examples.LinkRank.LinkRankVertexMasterCompute;
+import org.apache.giraph.examples.LinkRank.NutchTableEdgeInputFormat;
+import org.apache.giraph.examples.LinkRank.NutchTableEdgeOutputFormat;
+import org.apache.giraph.job.GiraphJob;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Abortable;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
+import org.apache.hadoop.hbase.zookeeper.ZooKeeperWatcher;
+import org.apache.hadoop.util.Tool;
+import org.apache.log4j.Logger;
+
+
+public class LinkRankGiraphJob implements Tool {
+  private static final Logger LOG = Logger.getLogger(LinkRankGiraphJob.class);
+  private GiraphConfiguration conf;
+  private static final String INPUT_TABLE_NAME = "webpage";
+  private static final String OUTPUT_TABLE_NAME = "webpage";
+
+  @Override
+  public int run(String[] strings) throws Exception {
+
+    LOG.info("Starting LinkRank Giraph Job");
+    Configuration config = HBaseConfiguration.create();
+    //config.clear();
+    config.set("hbase.zookeeper.quorum", "localhost");
+    config.set("hbase.zookeeper.property.clientPort", "2181");
+    config.set("hbase.master", "localhost:60000");
+    config.set("mapred.job.tracker", "localhost:9001");
+
+
+    HBaseAdmin admin = new HBaseAdmin(config);
+    ZooKeeperWatcher zooKeeperWatcher = new ZooKeeperWatcher(config, "zkw", new Abortable() {
+      @Override
+      public void abort(String s, Throwable throwable) {
+        System.out.println(s);
+      }
+    });
+
+    admin.getMaster();
+
+    GiraphConfiguration giraphConf = new GiraphConfiguration(config);
+    giraphConf.setZooKeeperConfiguration(
+            zooKeeperWatcher.getQuorum());
+    giraphConf.setComputationClass(LinkRankComputation.class);
+    giraphConf.setMasterComputeClass(LinkRankVertexMasterCompute.class);
+    giraphConf.setOutEdgesClass(ByteArrayEdges.class);
+    giraphConf.setVertexInputFormatClass(NutchTableEdgeInputFormat.class);
+    giraphConf.setVertexOutputFormatClass(NutchTableEdgeOutputFormat.class);
+    giraphConf.setInt("giraph.linkRank.superstepCount", 10);
+    giraphConf.setWorkerConfiguration(1, 1, 100.0f);
+    giraphConf.set(TableInputFormat.INPUT_TABLE, INPUT_TABLE_NAME);
+    giraphConf.set(TableOutputFormat.OUTPUT_TABLE, OUTPUT_TABLE_NAME);
+
+    GiraphJob giraphJob = new GiraphJob(giraphConf, "LinkRank");
+    return giraphJob.run(true) ? 0: -1;
+
+  }
+
+  @Override
+  public void setConf(final Configuration conf) {
+    this.conf = new GiraphConfiguration(conf);
+  }
+
+  @Override
+  public Configuration getConf() {
+    return conf;
+  }
+}
---
+++ src/java/org/apache/nutch/scoring/LinkRankJob.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.apache.nutch.scoring;
+
+import org.apache.avro.util.Utf8;
+import org.apache.giraph.io.formats.PseudoRandomIntNullVertexInputFormat;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapreduce.InputFormat;
+import org.apache.hadoop.mapreduce.lib.output.NullOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.nutch.metadata.Nutch;
+import org.apache.nutch.util.ToolUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class LinkRankJob {
+
+  public static final Logger LOG = LoggerFactory.getLogger(LinkRankJob.class);
+  private Configuration conf;
+
+  public LinkRankJob() {
+    LOG.info("constructor started");
+    Map<String, Object> args = null;
+    try {
+      LOG.info("running arg");
+      run(args);
+    } catch (Exception e) {
+      LOG.info("exception");
+      LOG.info(e.getMessage());
+      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+    }
+  }
+
+  public Map<String, Object> run(Map<String, Object> args) throws Exception {
+    LOG.info("LinkRank starts...");
+    LinkRankGiraphJob linkRankJob = new LinkRankGiraphJob();
+    linkRankJob.run(null);
+    LOG.info("LinkRank has finished...");
+    return null;
+  }
+  
+  private int updateTable() throws Exception {
+    LOG.info("ScoreUpdaterJob: starting");
+    run(ToolUtil.toArgMap(Nutch.ARG_CRAWL, 0));
+    LOG.info("ScoreUpdaterJob: done");
+    return 0;
+  }
+
+  public int run(String[] args) throws Exception {
+    return updateTable();
+  }
+
+  public static void main(String[] args) throws Exception {
+    //int res = ToolRunner.run(NutchConfiguration.create(), new ScoreUpdaterJob(), args);
+    new LinkRankJob();
+
+  }
+
+}
