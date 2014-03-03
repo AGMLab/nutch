9037bce57f675e59a6f9a54f85e27650bc7667fa
Cengiz Asmazoglu <cengizasmazoglu@gmail.com>
2014-03-03 10:16:04 +0200

    bugfix

--- src/java/org/apache/nutch/tools/TrustFlagLoader.java
+++ src/java/org/apache/nutch/tools/TrustFlagLoader.java
@@ -19,76 +19,70 @@ import java.io.*;
  * To change this template use File | Settings | File Templates.
  */
 public class TrustFlagLoader {
-
     private static String TABLE_NAME = "host";
 
-
-        public static void writeHostListToHBase(String[] args) throws IOException {
-            File inputFile = null;
-        	for (int i = 0; i < args.length; i++){
-            	if (args[i].equals("-f")) {
-            		inputFile = new File(args[i+1]);
-            	}
-            	if(args[i].equals("-p")){
-            		TABLE_NAME=args[i+1]+"_"+TABLE_NAME;
-            	}    	
-            }
-            BufferedReader bufferedReader = null;
-            Configuration conf = HBaseConfiguration.create();
-            HTable table = new HTable(conf, TABLE_NAME);
-
-
-            if (conf == null){
-                return;
-            }
-
-            try {
-                bufferedReader = new BufferedReader(new FileReader(inputFile));
-                String line = null;
-                String reversedHost=null;
-                String reversedUrl=null;
-                String trustFlag="1";
-
-                while ( (line = bufferedReader.readLine()) != null){
-                   String[] cols = line.split("\t");
-//                   reversedHost=reverseHost(cols[0]);
-                   reversedUrl = TableUtil.reverseUrl(cols[0]);
-                   reversedHost = TableUtil.getReversedHost(reversedUrl);
-                    Put p = new Put(Bytes.toBytes(reversedHost));
-                   p.add(Bytes.toBytes("mtdt"), Bytes.toBytes("_tf_"), Bytes.toBytes(trustFlag));
-                   table.put(p);
-
-
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            finally {
-                if (bufferedReader != null)
-                    try {
-                        bufferedReader.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-            }
+    public static void writeHostListToHBase(String[] args) throws IOException {
+        File inputFile = null;
+    	for (int i = 0; i < args.length; i++){
+        	if (args[i].equals("-f")) {
+        		inputFile = new File(args[i+1]);
+        	}
+        	if(args[i].equals("-p")){
+        		TABLE_NAME=args[i+1]+"_"+TABLE_NAME;
+        	}    	
         }
-        
-        /**
-         * @param host
-         * 
-         * example:
-         * http://meb.gov.tr
-         * http://milliyet.tv
-         * http://acunn.com
-         * @return reversedHost
-         * 
-         * example:
-         * tr.gov.meb.www
-         * tv.milliyet.www
-         * com.acunn.www
-         */
+        BufferedReader bufferedReader = null;
+        Configuration conf = HBaseConfiguration.create();
+        HTable table = new HTable(conf, TABLE_NAME);
+
+        if (conf == null){
+            return;
+        }
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(inputFile));
+            String line = null;
+            String reversedHost=null;
+            String reversedUrl=null;
+            String trustFlag="1";
+
+            while ( (line = bufferedReader.readLine()) != null){
+               String[] cols = line.split("\t");
+               reversedUrl = TableUtil.reverseUrl(cols[0]);
+               reversedHost = TableUtil.getReversedHost(reversedUrl);
+                Put p = new Put(Bytes.toBytes(reversedHost));
+               p.add(Bytes.toBytes("mtdt"), Bytes.toBytes("_tf_"), Bytes.toBytes(trustFlag));
+               table.put(p);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (bufferedReader != null)
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+    
+    /**
+     * @param host
+     * 
+     * example:
+     * http://meb.gov.tr
+     * http://milliyet.tv
+     * http://acunn.com
+     * @return reversedHost
+     * 
+     * example:
+     * tr.gov.meb.www
+     * tv.milliyet.www
+     * com.acunn.www
+     */
 //        public static String reverseHost(String host){
 //        	host= host.substring(7);
 //        	String[] pieces = StringUtils.split(host, '.');
@@ -103,18 +97,15 @@ public class TrustFlagLoader {
 //        	return buf.toString();
 //        }
 
-        public static void main(String[] args){
-
-            if (args.length < 1){
-                System.err.println("Usage: TrustFlagLoader -f <host_file> -p <output_table_prefix>");
-                return;
-            }
-
-            try {
-                writeHostListToHBase(args);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    public static void main(String[] args){
+        if (args.length < 1){
+            System.err.println("Usage: TrustFlagLoader -f <host_file> -p <output_table_prefix>");
+            return;
         }
-
+        try {
+            writeHostListToHBase(args);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
